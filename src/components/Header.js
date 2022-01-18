338bdccf537a517436a1ce628907e278e2580a0e
rizwan230698 <rizwananwar230698@gmail.com>
2022-01-18 17:54:04 +0530

    added shadow and removed border bottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Button from "./Button";
 const Header = () => {
   const { isUser, logout } = useContext(UserContext);
   return (
-    <div className="flex items-center justify-end px-5 lg:px-14 h-16 bg-white border-b border-gray-300">
+    <div className="flex items-center justify-end px-5 lg:px-14 h-16 bg-white shadow-sm">
       {isUser && (
         <Button mt="mt-0 lg:mt-0" h="h-10" px="px-8" onClick={logout}>
           Log out
